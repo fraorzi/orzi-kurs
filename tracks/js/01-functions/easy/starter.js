367ffa3ee18db859890415733e8fc57f481bab77
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-07-14 17:18:37 +0200

    solve: js/01-functions/easy

--- tracks/js/01-functions/easy/starter.js
+++ tracks/js/01-functions/easy/starter.js
@@ -1,11 +1,16 @@
 export function min(a, b) {
-  // TODO
+    if (a < b) return a;
+    return b;
 }
 
 export function pow(x, n) {
-  // TODO
+    let sum = 1;
+    for (let i = 0; i < n; i++) {
+        sum *= x;
+    }
+    return sum;
 }
 
-export function greet(name, greeting) {
-  // TODO
+export function greet(name, greeting = "Cześć") {
+    return `${greeting}, ${name}!`;
 }
