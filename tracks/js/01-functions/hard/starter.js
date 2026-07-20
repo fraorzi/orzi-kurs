18bac174cde9a705c25e111f7508ed082f5bdb41
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-07-20 21:08:26 +0200

    solve: js/01-functions/hard

--- tracks/js/01-functions/hard/starter.js
+++ tracks/js/01-functions/hard/starter.js
@@ -1,9 +1,9 @@
 export function spy(fn) {
-  wrapper.calls = [];
   function wrapper(...args) {
     wrapper.calls.push(args);
     return fn.apply(this, args);
   }
+  wrapper.calls = [];
   return wrapper;
 }
 
