c5a3834fc04f2e0c73415139d32fc28542dc8e7e
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-07-21 18:16:22 +0200

    solve: js/03-types-coercion/medium

--- tracks/js/03-types-coercion/medium/starter.js
+++ tracks/js/03-types-coercion/medium/starter.js
@@ -1,7 +1,17 @@
 export function sameValue(a, b) {
-  // TODO
+  if (a === b) {
+    return a !== 0 || 1 / a === 1 / b;
+  }
+  return a !== a && b !== b;
 }
 
 export function defaultTo(value, fallback) {
-  // TODO
+  if (
+    value === undefined ||
+    value === null ||
+    Number.isNaN(value)
+  ) {
+    return fallback;
+  }
+  return value;
 }
