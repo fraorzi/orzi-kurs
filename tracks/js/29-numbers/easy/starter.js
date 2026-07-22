ce023ae2c2b9069aa28189f670f063c1ae831c30
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-07-22 23:38:23 +0200

    solve: js/29-numbers/easy

--- tracks/js/29-numbers/easy/starter.js
+++ tracks/js/29-numbers/easy/starter.js
@@ -1,7 +1,9 @@
 export function roundTo(value, decimals) {
-  // TODO: zaokrąglij do decimals miejsc, zwróć liczbę (Math.round(value * 10**decimals) / 10**decimals)
+  return (
+    Math.round(value * 10 ** decimals) / 10 ** decimals
+  );
 }
 
 export function isInteger(value) {
-  // TODO: Number.isInteger(value)
+  return Number.isInteger(value);
 }
