366bdacaa21e42a42eb48980813748d80021c1bb
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-07-31 13:01:09 +0200

    solve: js/04-loops/easy

--- tracks/js/04-loops/easy/starter.js
+++ tracks/js/04-loops/easy/starter.js
@@ -1,11 +1,38 @@
 export function fizzBuzz(n) {
-  // TODO
+  const res = [];
+  function isFizz(number) {
+    return number % 3 === 0;
+  }
+  function isBuzz(number) {
+    return number % 5 === 0;
+  }
+  for (let i = 1; i <= n; i++) {
+    if (isFizz(i) && isBuzz(i)) {
+      res.push("FizzBuzz");
+    } else if (isBuzz(i)) {
+      res.push("Buzz");
+    } else if (isFizz(i)) {
+      res.push("Fizz");
+    } else {
+      res.push(i);
+    }
+  }
+  return res;
 }
 
 export function sumRange(a, b) {
-  // TODO
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 export function countVowels(str) {
-  // TODO
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (const letter of str.toLowerCase()) {
+    if (vowels.includes(letter)) count++;
+  }
+  return count;
 }
