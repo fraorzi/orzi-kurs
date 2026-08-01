014da199d0d57f34e48c60dc2d81f4a224151d72
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-01 16:38:06 +0200

    solve: js/04-loops/medium

--- tracks/js/04-loops/medium/starter.js
+++ tracks/js/04-loops/medium/starter.js
@@ -1,11 +1,38 @@
 export function primesUpTo(n) {
-  // TODO
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+
+  const res = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) res.push(i);
+  }
+  return res;
 }
 
 export function chessboard(size) {
-  // TODO
+  let string = "";
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const iteracio = j + i;
+      if (iteracio % 2 === 0) {
+        string += " ";
+      } else if (iteracio % 2 === 1) {
+        string += "#";
+      }
+    }
+    string += "\n";
+  }
+  return string;
 }
 
 export function firstIndexWhere(arr, pred) {
-  // TODO
+  for (let i = 0; i < arr.length; i++) {
+    if (pred(arr[i], i)) return i;
+  }
+  return -1;
 }
