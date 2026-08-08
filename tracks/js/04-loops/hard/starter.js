d5e26834602c8797fce7effbd587f39e48663bc1
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-08 15:14:50 +0200

    solve: js/04-loops/hard

--- tracks/js/04-loops/hard/starter.js
+++ tracks/js/04-loops/hard/starter.js
@@ -1,7 +1,29 @@
 export function sieve(n) {
-  // TODO
+  const res = [];
+  res.length = n + 1;
+  for (let i = 0; i <= n; i++) {
+    res[i] = i;
+  }
+  for (let j = 2; j * j <= n; j++) {
+    if (res[j] !== false) {
+      for (let k = j * j; k <= n; k += j) {
+        res[k] = false;
+      }
+    }
+  }
+  res[1] = false;
+  return res.filter(Boolean);
 }
 
 export function collatzLength(n) {
-  // TODO
+  let count = 1;
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+    count++;
+  }
+  return count;
 }
