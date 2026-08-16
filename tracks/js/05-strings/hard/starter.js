aa6ab6f682f6bc64f8c212ec176b8cbdd76169bc
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-16 03:28:23 +0200

    solve: js/05-strings/hard

--- tracks/js/05-strings/hard/starter.js
+++ tracks/js/05-strings/hard/starter.js
@@ -1,7 +1,50 @@
 export function findAnagrams(word, candidates) {
-  // TODO
+  const sortAndSplitLetters = (string) =>
+    string.toLowerCase().split("").sort();
+  const wordLowerCased = word.toLowerCase();
+  const formattedWord = sortAndSplitLetters(word);
+  const formattedCandidates = candidates.map((c) =>
+    sortAndSplitLetters(c),
+  );
+  const res = [];
+
+  for (let i = 0; i < candidates.length; i++) {
+    const isAnagram =
+      formattedWord.length ===
+        formattedCandidates[i].length &&
+      formattedWord.every(
+        (letter, index) =>
+          letter === formattedCandidates[i][index],
+      ) &&
+      wordLowerCased !== candidates[i].toLowerCase();
+    if (isAnagram) {
+      res.push(candidates[i]);
+    }
+  }
+
+  return res;
 }
 
 export function slugify(title) {
-  // TODO
+  let wynik = title
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replaceAll("+", "")
+    .replaceAll("=", "")
+    .replaceAll("  ", " ")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[!@#]/g, "")
+    .replace(/^-+/, "")
+    .replaceAll("--", "-")
+    .replaceAll("ł", "l")
+    .replaceAll(",", "")
+    .split(" ")
+    .join("-");
+
+  while (wynik.endsWith("-")) {
+    wynik = wynik.slice(0, -1);
+  }
+
+  return wynik;
 }
