dadcbb65f9f730004e8e449fee20446541dd5cd7
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-17 01:34:22 +0200

    solve: js/06-objects/easy

--- tracks/js/06-objects/easy/starter.js
+++ tracks/js/06-objects/easy/starter.js
@@ -1,11 +1,17 @@
 export function countProps(obj) {
-  // TODO
+  return Object.keys(obj).length;
 }
 
 export function sumSalaries(salaries) {
-  // TODO
+  return Object.values(salaries).reduce(
+    (sum, salary) => sum + salary,
+    0,
+  );
 }
 
 export function renameKey(obj, from, to) {
-  // TODO
+  const entries = Object.entries(obj).map(
+    ([key, value]) => [key === from ? to : key, value],
+  );
+  return Object.fromEntries(entries);
 }
