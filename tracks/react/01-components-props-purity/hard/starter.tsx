97bf8cff64c3618fec6f89fd3883700ef742494c
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-18 21:28:42 +0200

    solve: react/01-components-props-purity/hard

--- tracks/react/01-components-props-purity/hard/starter.tsx
+++ tracks/react/01-components-props-purity/hard/starter.tsx
@@ -8,6 +8,26 @@ export interface ProductTableProps {
   readonly products: readonly Product[];
 }
 
-export function ProductTable(_props: ProductTableProps) {
-  return <section>TODO: cennik</section>;
+export function ProductTable({
+  products,
+}: ProductTableProps) {
+  const sortedProducts = [...products].sort(
+    (a, b) => a.price - b.price,
+  );
+  const sum = sortedProducts.reduce(
+    (sum, product) => sum + product.price,
+    0,
+  );
+  return (
+    <section aria-label="Cennik">
+      <ol>
+        {sortedProducts.map((product) => (
+          <li key={product.id}>
+            {product.name}: {product.price.toFixed(2)} zł
+          </li>
+        ))}
+      </ol>
+      <p>Razem: {sum.toFixed(2)} zł</p>
+    </section>
+  );
 }
