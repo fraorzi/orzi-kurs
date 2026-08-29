d4bd25d02dae78393cd39aeeb1c5da0ae6b4b733
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-08-29 21:37:25 +0200

    solve: js/06-objects/medium

--- tracks/js/06-objects/medium/starter.js
+++ tracks/js/06-objects/medium/starter.js
@@ -1,11 +1,29 @@
 export function multiplyNumeric(obj) {
-  // TODO
+  const result = {};
+  for (const key in obj) {
+    if (typeof obj[key] === "number") {
+      result[key] = obj[key] * 2;
+    } else {
+      result[key] = obj[key];
+    }
+  }
+  return result;
 }
 
 export function pick(obj, keys) {
-  // TODO
+  const result = {};
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
 }
 
 export function invert(obj) {
-  // TODO
+  const result = {};
+  for (const key in obj) {
+    result[obj[key]] = key;
+  }
+  return result;
 }
