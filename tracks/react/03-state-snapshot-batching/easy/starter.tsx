c3b14ff22f5ae7a47a551b7f2724e1a0caecfec8
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2026-09-02 00:22:42 +0200

    solve: react/03-state-snapshot-batching/easy

--- tracks/react/03-state-snapshot-batching/easy/starter.tsx
+++ tracks/react/03-state-snapshot-batching/easy/starter.tsx
@@ -4,12 +4,19 @@ export interface ToggleDetailsProps {
   readonly details: string;
 }
 
-export function ToggleDetails({ details }: ToggleDetailsProps) {
-  const [isOpen] = useState(false);
+export function ToggleDetails({
+  details,
+}: ToggleDetailsProps) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <section>
-      <button type="button">Pokaż szczegóły</button>
+      <button
+        type="button"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {!isOpen ? "Pokaż szczegóły" : "Ukryj szczegóły"}
+      </button>
       {isOpen ? <p>{details}</p> : null}
     </section>
   );
